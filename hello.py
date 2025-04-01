c56499a4079f8905f5206e59212f4e6a4a9ca18c
SeiamAlMahmud <samahmud49@gmail.com>
2025-04-02 00:46:54 +0600

    Identifier

--- hello.py
+++ hello.py
@@ -1,7 +1,13 @@
 name = "Seiam al mahmud"
 age = 25
 price = 2000.25
+status = True
+list = []
+a = None
 print(name, "And my age is 25.",2 *2000.25, +5)
+print(type(a))
+print(type(list))
+print(type(status))
 print(type(name))
 print(type(age))
 print(type(price))
