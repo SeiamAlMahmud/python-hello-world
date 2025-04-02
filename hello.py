fba3e353a7e5dab977d553908b18ce132b6d7c84
SeiamAlMahmud <samahmud49@gmail.com>
2025-04-02 11:45:00 +0600

    chapter 1 on apna college

--- hello.py
+++ hello.py
@@ -1,13 +1,26 @@
-name = "Seiam al mahmud"
-age = 25
-price = 2000.25
-status = True
-list = []
-a = None
-print(name, "And my age is 25.",2 *2000.25, +5)
-print(type(a))
-print(type(list))
-print(type(status))
-print(type(name))
-print(type(age))
-print(type(price))
+# name = "Seiam al mahmud"
+# age = 25
+# price = 2000.25
+# sum = age - price
+# status = True
+# list = []
+# a = None
+# print(name, "And my age is 25.",2 *2000.25, +5)
+# print(sum)
+# print(sum)
+# print(type(a))
+# print(type(list))
+# print(type(status))
+# print(type(name))
+# print(type(age))
+# print(type(price))
+
+# num = 12
+# f =6
+# # num **= 5
+# print((num / f))
+
+# name = input("Enter your name:" )
+a = int(input("a "))
+b = int(input("b "))
+print((a + b)/2)
