e6d47a956553e22f81e385695e66bd72035ca226
SeiamAlMahmud <samahmud49@gmail.com>
2025-04-03 01:11:24 +0600

    lecture two

---
+++ lecture_two.py
@@ -0,0 +1,51 @@
+str1 = "this is a string.\nwe are creating it in python."
+str2 = "This is a string.\twe are creating it in python."
+# print(str1)
+# print(str2)
+# print(len(str1))
+# print(str1,str2)
+# print(str1[6:len(str1)])
+
+# print(str1.endswith("n.")) # returns true if string ends with sybstr
+# print(str1.capitalize()) # capitalizes 1st char 
+# print(str2.replace("python","javascript")) # replaces all occurences of old word with new word
+
+# print(str2.find("a")) #returns 1st index of 1st occurrer
+# print(str2.find("zzz")) #returns -1 if not found 
+
+
+# print(str2.count("a")) # counts the occurrence of substr
+# print(str2.count("ammj")) # returns  0 if not found for counting
+# 
+# 
+
+
+
+
+
+# age = 25
+# if(age >= 18):
+#     print("can vote & apply for license")
+#     print("can vote")
+#     print("apply for license")
+
+# light = "blue"
+# if(light == "red"):
+#     print("stop")
+# elif(light == "green"):
+#     print("go")
+# elif(light == "yellow"):
+#     print("look")   # indentation {spaces}
+# else:
+#     print("Lisght is brokern")
+
+
+# Nesting
+# age = 95
+# if(age >= 18):
+#     if(age >=80):
+#         print("cannot drive")
+#     else:
+#         print("can drive")
+# else: 
+#     print("can not drive")
