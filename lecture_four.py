121a3bdaf6eaaea8ed1b9a05fb9c0e062ec14770
SeiamAlMahmud <samahmud49@gmail.com>
2025-04-04 23:17:28 +0600

    lecture four

--- lecture_four.py
+++ lecture_four.py
@@ -65,3 +65,80 @@ student = {
 
 
 
+
+
+
+
+
+collection = {1,2,3,4,2,2, "world","world"} #unordered
+
+# print(collection)
+# print(len(collection))
+# print(type(collection))
+
+emtryCollection = set()
+# print(emtryCollection) # set()
+# print(type(emtryCollection))
+
+
+newCollection = set()
+newCollection.add(1)
+newCollection.add(2)
+newCollection.add(2)
+newCollection.add(3)
+# print(newCollection) # {1, 2}
+
+
+# newCollection.remove(1)
+# print(newCollection) # {2}
+
+
+# newCollection.clear()
+# print(newCollection.pop()) # random pick and remove
+# print(newCollection.pop()) # random pick and remove
+# print(newCollection) # set()
+
+
+
+set1 = {1,2,3}
+set2 = {2,3,4}
+# set.union(set1)  # combines both set values & returns new
+# set.intersection(set1)  # combines values & returns new
+
+
+# print(set1.union(set2)) # {1, 2, 3, 4}
+# print(set1.intersection(set2)) # {2, 3}
+
+
+# Store following word meanings in a python dictionary: 
+# table: "a piece of furniture", "list of facts & figures"
+# "cat": "a small animal"
+
+dictionary = {
+    "cat": "a small animal",
+    "table": ["a piece of furniture", "list of facts & figures"]
+}
+
+# print(dictionary)
+
+
+# You are given a list o subjescts for students. assume one classroom is required for  1 subject. How many classrooms are neded by all students.
+    
+#     "python", "java", "C++", "python", "javascript", "java", "python", "java", "C++", "C", 
+
+subjects = {
+    "python", "java", "C++", "python", "javascript", "java", "python", "java", "C++", "C", 
+}
+
+# print(len(subjects)) # 5
+
+# values = {9, 9.0, 9.25}
+# print(values)
+# values = {9, '9.0', 9.25}
+# print(values)
+
+values ={
+    ("float", 9.0),
+    ("int", 9),
+}
+print(values)
