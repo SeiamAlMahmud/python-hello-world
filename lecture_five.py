b9be2ff490feb82c433f0ba4a8cd27ea4695adb7
SeiamAlMahmud <samahmud49@gmail.com>
2025-04-05 00:14:26 +0600

    while loop

---
+++ lecture_five.py
@@ -0,0 +1,63 @@
+# count = 1
+# while count <= 500 :
+#     print("hello world")
+#     count += 1
+
+# print(count)
+
+
+
+#print numbers from 1 to 5
+# i = 5
+# while i >= 1 :
+#     print(i)
+#     i -= 1
+
+# print(i)
+
+# Print numbers from 1 to 100
+# i = 1
+# while i <= 100:
+#     print(i)
+#     i +=1
+
+
+# Print numbers from 100 to 1.
+# i = 100
+# while i >= 1:
+#     print(i)
+#     i -=1
+
+
+# Print the multiplicaiton table of a number n.
+# n = 3
+# count = 1
+# while count <= 10:
+#     print(count * n)
+#     count +=1
+
+
+# Print the elements of the following list using a loop:
+# numbs = [1,4,9,16,25,36,49,64,81,100]
+
+# idx = 0
+# while idx < len(numbs):
+#     print(numbs[idx])
+#     idx +=1
+
+
+# Search for a number x in this tuple using loop
+
+numbs = (1,4,9,16,25,36,49,64,25,81,100)
+# x = 25
+# idx = 0
+# while idx < len(numbs):
+#     if numbs[idx] == x:
+#         print("Found at index", idx)
+#         # break
+#     else:
+#         print("Finding...")
+#     idx +=1
+
+
+
