6abacd68ce6ece828c71879b15b603426e27d52a
SeiamAlMahmud <samahmud49@gmail.com>
2025-04-11 23:31:01 +0600

    recursive function

--- lecture_six.py
+++ lecture_six.py
@@ -57,5 +57,43 @@ def show(n):
     print(n)
     show(n-1)
 
-show(5) # call stack
+# show(5) # call stack
   
+
+# return n!
+
+def fact(n):
+    if(n == 0 or n == 1):
+        return 1
+    else:
+        return n * fact(n - 1)
+    
+# print(fact(1))
+
+
+# write a recursive function to calculate the sum of first n natural mumbers.
+
+def calc_sum(n):
+    if(n == 0):
+        return 0
+    return calc_sum(n-1) + n
+
+
+sum = calc_sum(10)
+
+# print(sum)
+
+
+# write a recursive function to print all elements in a list. 
+# Hint: use list & index as parameters
+
+def print_list(list, idx = 0):
+    if(idx == len(list)):
+        return
+    print(list[idx])
+    print_list(list, idx+1)
+
+
+fruits = ["mango", "litchi", "apple", "banana"]
+
+print_list(fruits)
