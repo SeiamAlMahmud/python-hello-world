cc86b0f3b3b94fd884f5634a9c0e1328e290485e
SeiamAlMahmud <samahmud49@gmail.com>
2025-12-28 00:03:09 +0600

    code with harray , learning py

---
+++ chapter-2/dataTypes.py
@@ -0,0 +1,16 @@
+a = 1 # a is an integer
+b = 5.22  # a is an floating point number
+
+c = "Harry"  # c is an string
+
+d = False # d is an boolean variable
+
+e = None # e is a none type variable
+
+
+
+
+
+
+
+
